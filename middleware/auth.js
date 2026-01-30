007aae74cfd041d05de4c1399446567dca94b47f
Skandan69 <skandan.kovuri@gmail.com>
2026-01-30 11:35:13 +0530

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function(req, res, next) {
+  const header = req.headers.authorization;
+
+  if (!header) return res.status(401).json({ message: "No token provided" });
+
+  const token = header.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "knowledgehubsecret"
+    );
+
+    req.adminId = decoded.id;
+    next();
+
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
