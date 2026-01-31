ffe6b09d79fca8487751d19e4102ee749e8b89d5
Skandan69 <skandan.kovuri@gmail.com>
2026-01-31 21:28:59 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,27 +1,38 @@
 const mongoose = require("mongoose");
 
 const AdminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+
+  name: { 
+    type: String, 
+    required: true 
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
+
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
   },
-  password: {
-    type: String,
-    required: true
+
+  password: { 
+    type: String, 
+    required: true 
   },
+
   role: {
     type: String,
-    default: "admin"
+    default: "admin" // admin or superadmin
   },
-  createdAt: {
-    type: Date,
-    default: Date.now
+
+  department: {
+    type: String,
+    default: null   // Only admins get department
+  },
+
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
   }
+
 });
 
 module.exports = mongoose.model("Admin", AdminSchema);
